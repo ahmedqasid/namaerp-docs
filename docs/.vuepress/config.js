edeb9e76cb8cd1060160b14fa038ae3937fbb76c
mahmoudyasser <mahmoudyaser3110@gmail.com>
2025-05-13 02:44:18 +0300

    Initial Commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  lang: 'en-US',
+
+  title: 'Nama ERP Docs',
+  description: 'Nama ERP Documentation',
+
+  theme: defaultTheme({
+    logo: 'https://vuejs.press/images/hero.png',
+
+    navbar: ['/', '/get-started'],
+  }),
+
+  bundler: viteBundler(),
+})
