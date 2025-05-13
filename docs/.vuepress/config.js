71b5d71ec5d953935a3dc0c7712728a658a7fd89
ahmedqasid <ahmad.kassyd@gmail.com>
2025-05-13 03:05:09 +0300

    added support for rtl

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,40 @@
 import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
+import {rtlPlugin} from "@vuepress/plugin-rtl";
 
 export default defineUserConfig({
-  lang: 'en-US',
-
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'Nama ERP Docs',
+      description: 'Nama ERP Documentation',
+    },
+    '/ar/': {
+      lang: 'ar-EG',
+      title: 'Nama ERP Docs',
+      description: 'Nama ERP Documentation',
+    },
+  },
   title: 'Nama ERP Docs',
   description: 'Nama ERP Documentation',
 
   theme: defaultTheme({
     logo: 'https://vuejs.press/images/hero.png',
-
+    locales: {
+      '/': {
+        selectLanguageName: 'English',
+      },
+      '/ar/': {
+        selectLanguageName: 'العربية',
+      },
+    },
     navbar: ['/', '/get-started'],
   }),
-
+  plugins: [
+    rtlPlugin({
+      locales: ['/ar/'],
+    }),
+  ],
   bundler: viteBundler(),
 })
