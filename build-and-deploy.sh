5c492e7f374fe1cecde3c91fafb96bf1420c098f
Ahmed Qasid <ahmad.kassyd@gmail.com>
2025-05-14 04:04:59 +0300

    build and deploy script

---
+++ build-and-deploy.sh
@@ -0,0 +1,26 @@
+set -x
+function git_pull() {
+    git reset --hard
+    git checkout -B master origin/master
+    git checkout -f master
+    git pull  origin master
+    #git gc
+}
+function checkForErrors() {
+    rc=$?
+    if [[ $rc != 0 ]] ; then
+      echo 'There are build errors';
+      ./notify.sh NOTOK $RELEASE_NAME
+      touch /var/tmp/namabuilderrors.txt
+      exit
+    fi
+}
+git_pull
+checkForErrors
+npm i
+npm run docs:build
+checkForErrors
+mkdir /var/www/docs
+rsync -L -r -t -v -z -q --delete -s  ./docs/.vuepress/dist  /var/www/html/docs/
+chown -R www-data /var/www/html/docs/
+
