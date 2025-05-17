fe64edceaed101d8034602a230d82b6af187bd51
Ahmed Qasid <ahmad.kassyd@gmail.com>
2025-05-18 01:05:26 +0300

    removed locales

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,30 +5,10 @@ import {SIDEBAR_CONFIG} from "./sidebar.js";
 import {slimsearchPlugin} from "@vuepress/plugin-slimsearch";
 
 export default defineUserConfig({
-  locales: {
-    '/': {
-      lang: 'en-US',
-      title: 'Nama ERP Docs',
-      description: 'Nama ERP Documentation',
-    },
-    '/ar/': {
-      lang: 'ar-EG',
-      title: 'Nama ERP Docs',
-      description: 'Nama ERP Documentation',
-    },
-  },
   title: 'Nama ERP Docs',
   description: 'Nama ERP Documentation',
 
   theme: defaultTheme({
-    locales: {
-      '/': {
-        selectLanguageName: 'English',
-      },
-      '/ar/': {
-        selectLanguageName: 'العربية',
-      },
-    },
     sidebar: SIDEBAR_CONFIG,
     navbar: ['/', '/guide/','/examples/'],
   }),
