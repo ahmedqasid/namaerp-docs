203a2fd5bf601936a709cf63df1fc9bed81e43b5
Ahmed Qasid <ahmad.kassyd@gmail.com>
2025-05-18 00:15:03 +0300

    no rtl in server mode

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,9 +1,11 @@
-import {defineClientConfig} from 'vuepress/client'
+import { defineClientConfig } from '@vuepress/client'
 
 export default defineClientConfig({
     enhance({app, router, siteData}) {
         router.afterEach((to) => {
             // console.log(to.meta._pageChunk.data.frontmatter.lang)
+            if(typeof window === 'undefined')
+                return;
             const isRtl = to.path.startsWith('/ar/') || to.meta._pageChunk?.data?.frontmatter?.lang === 'ar'
             document.documentElement.setAttribute('dir', isRtl ? 'rtl' : 'ltr')
         })
