a97d1f8347cc1f47480eba54925fb20349779d5b
Ahmed Qasid <ahmad.kassyd@gmail.com>
2025-06-13 00:06:14 +0300

    added video tutorials

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -26,6 +26,10 @@ export const SIDEBAR_CONFIG = {
       text: "Frequently Asked Questions",
       collapsible: true,
       children: [ "/faq/general-faq.md", "/faq/entity-flow-faq.md", "/faq/supply-chain-faq.md", "/faq/human-resources-faq.md", "/faq/report-faq.md", "/faq/report-wizard-faq.md", "/faq/approvals-faq.md", "/faq/mobile-apps-faq.md", "/faq/field-filter-faq.md" ]
+    }, {
+      text: "Video Tutorials",
+      collapsible: true,
+      children: [ "/videos/report-wizard-webinar-1.md", "/videos/report-wizard-webinar-2.md", "/videos/report-wizard-webinar-3.md", "/videos/report-wizard-webinar-4.md", "/videos/report-wizard-webinar-5.md", "/videos/report-wizard-webinar-6.md" ]
     } ]
   } ]
 }
